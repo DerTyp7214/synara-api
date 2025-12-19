3a2f84ebf06bec8ac3fcbce3a4b7239b11168515
DerBot7214 <github@dertyp.dev>
2025-12-19 13:36:30 +0100

    chore: update Kotlin, Ktor, and dependencies to latest versions
    - Upgrade Kotlin to 2.2.21 across build scripts and dependencies.
    - Update Ktor to 3.3.3 alongside associated plugins and modules.
    - Bump multiple library versions, including Flyway, Jedis, and ktor-openapi.
    - Replace dynamic version handling in `build.gradle.kts` with static values for improved clarity and maintainability.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,20 +2,11 @@
 
 import io.ktor.plugin.*
 
-val exposedVersion: String by project
-val h2Version: String by project
-val kotlinVersion: String by project
-val kotlinxHtmlVersion: String by project
-val kotlinxRpcVersion: String by project
-val logbackVersion: String by project
-val postgresVersion: String by project
-val jmdnsVersion: String by project
-
 plugins {
-    kotlin("jvm") version "2.2.20"
-    id("io.ktor.plugin") version "3.3.1"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20"
-    id("org.jetbrains.kotlinx.rpc.plugin") version "0.10.0"
+    kotlin("jvm") version "2.2.21"
+    id("io.ktor.plugin") version "3.3.3"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21"
+    id("org.jetbrains.kotlinx.rpc.plugin") version "0.10.1"
 }
 
 application {
@@ -44,15 +35,15 @@ dependencies {
     implementation("io.ktor:ktor-client-cio")
     implementation("io.ktor:ktor-server-cors")
     implementation("io.ktor:ktor-server-core")
-    implementation("org.flywaydb:flyway-core:11.19.0")
-    implementation("org.flywaydb:flyway-database-postgresql:11.19.0")
+    implementation("org.flywaydb:flyway-core:11.19.1")
+    implementation("org.flywaydb:flyway-database-postgresql:11.19.1")
     implementation("com.zaxxer:HikariCP:7.0.2")
     implementation("com.ucasoft.ktor:ktor-simple-cache:0.55.3")
     implementation("com.ucasoft.ktor:ktor-simple-memory-cache:0.55.3")
     implementation("com.ucasoft.ktor:ktor-simple-redis-cache:0.55.3")
-    implementation("redis.clients:jedis:7.0.0")
-    implementation("io.github.smiley4:ktor-openapi:5.1.0")
-    implementation("io.github.smiley4:ktor-swagger-ui:5.1.0")
+    implementation("redis.clients:jedis:7.2.0")
+    implementation("io.github.smiley4:ktor-openapi:5.4.0")
+    implementation("io.github.smiley4:ktor-swagger-ui:5.4.0")
     implementation("io.ktor:ktor-server-auth")
     implementation("io.ktor:ktor-server-auth-jwt")
     implementation("io.ktor:ktor-server-sse")
@@ -66,36 +57,36 @@ dependencies {
     implementation("io.ktor:ktor-serialization-gson")
     implementation("io.ktor:ktor-serialization-kotlinx-protobuf")
     implementation("io.ktor:ktor-server-html-builder")
-    implementation("org.jetbrains.kotlinx:kotlinx-html:$kotlinxHtmlVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-html:0.12.0")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    implementation("com.h2database:h2:$h2Version")
+    implementation("org.jetbrains.exposed:exposed-dao:0.61.0")
+    implementation("org.jetbrains.exposed:exposed-core:0.61.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.61.0")
+    implementation("com.h2database:h2:2.3.232")
     implementation("org.bytedeco:ffmpeg-platform:7.1.1-1.5.12")
     implementation("org.bytedeco:javacv-platform:1.5.12")
     implementation("net.coobird:thumbnailator:0.4.21")
-    implementation("org.postgresql:postgresql:$postgresVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-server:$kotlinxRpcVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-client:$kotlinxRpcVersion")
+    implementation("org.postgresql:postgresql:42.7.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-server:0.10.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-client:0.10.0")
     implementation("io.ktor:ktor-server-netty")
     implementation("at.favre.lib:bcrypt:0.10.2")
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("ch.qos.logback:logback-classic:1.5.22")
     implementation("io.ktor:ktor-server-config-yaml")
     implementation("net.jthink:jaudiotagger:3.0.1")
     implementation("org.xerial:sqlite-jdbc:3.50.3.0")
-    implementation("io.ktor:ktor-server-compression:3.3.1")
-    implementation("io.ktor:ktor-server-auto-head-response:3.3.1")
-    implementation("io.ktor:ktor-server-sessions:3.3.1")
-    implementation("com.github.ben-manes.caffeine:caffeine:3.2.2")
-    implementation("io.ktor:ktor-server-partial-content:3.3.1")
-    implementation ("org.jmdns:jmdns:$jmdnsVersion")
+    implementation("io.ktor:ktor-server-compression:3.3.3")
+    implementation("io.ktor:ktor-server-auto-head-response:3.3.3")
+    implementation("io.ktor:ktor-server-sessions:3.3.3")
+    implementation("io.ktor:ktor-server-partial-content:3.3.3")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.2.3")
+    implementation ("org.jmdns:jmdns:3.6.1")
     implementation("com.sksamuel.scrimage:scrimage-core:4.3.5")
     implementation("com.sksamuel.scrimage:scrimage-webp:4.3.5")
-    implementation("io.insert-koin:koin-ktor:4.1.2-Beta1")
-    implementation("io.insert-koin:koin-logger-slf4j:4.1.2-Beta1")
+    implementation("io.insert-koin:koin-ktor:4.2.0-beta2")
+    implementation("io.insert-koin:koin-logger-slf4j:4.2.0-beta2")
     testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.2.21")
 }
 
 val ktorBaseImageTag = "synara-api-base:latest"
